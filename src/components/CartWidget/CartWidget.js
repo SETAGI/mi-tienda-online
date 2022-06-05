6e105d6bffaa12efa184d5783975f5bd276458ec
SETAGI <tamayo0711@hotmail.com>
2022-06-04 22:06:50 -0500

    Sync counter

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './CartWidget.css'
 
-const CartWidget = ({amount}) => {
+const CartWidget = ({amount = 0}) => {
     return (
         <div className='container'>
             <img className='cartWidget' src='../images/cart.svg' alt='cart-widget'/>
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { getItemById } from '../../utils/asyncMockProducts';
-import ItemCount from '../ItemCount/ItemCount';
 import { useParams } from 'react-router-dom';
 
-const ItemDetailContainer = ({setSelectedAmount}) => {
+const ItemDetailContainer = () => {
     const { itemId } = useParams();
     const [item, setItem]= useState({});
-    const onAdd = amount => setSelectedAmount( amount )
 
     useEffect( () => {
         getItemById(itemId).then(res => setItem(res))
     },[itemId])
 
-    return(
-        <>
-            <ItemDetail {...item} />
-            <ItemCount stock={ 10 } initial={ 1 } onAdd={onAdd} />  
-        </>
-    ) 
+    return <ItemDetail {...item} /> 
 }
 
 export default ItemDetailContainer
